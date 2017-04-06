7a015c220273e5633e26af027b3f0d96707e3c91
小刘童鞋 <lgr597909952@163.com>
2017-04-06 12:19:21 +0800

    修改我的回答缓存问题，修正跳转缓存问题

--- www/js/utilityMemoAnswersCtrl.js
+++ www/js/utilityMemoAnswersCtrl.js
@@ -1,6 +1,11 @@
-angular.module('ionicApp.MemoAnswersCtrl', []).controller("MemoAnswersCtrl",['Memos','$scope',
-function (Memos,$scope) {
+angular.module('ionicApp.MemoAnswersCtrl', []).controller("MemoAnswersCtrl",['Memos','$scope','$state',
+function (Memos,$scope,$state) {
     Memos.GetAnwsers_page().then(function(anwsers){
         $scope.anwsers = anwsers;
     });
-    }]);
+    //跳转
+    $scope.GoContent=function(id){
+        localStorage.setItem(set_question_id,id);
+        $state.go("tab.memo-content");
+    };
+}]);
