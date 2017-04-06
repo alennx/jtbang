cbbd30882ddf932c2704e11e7e653fce46af367d
小刘童鞋 <lgr597909952@163.com>
2017-04-06 13:56:43 +0800

    我的回答、我的提问bug解决

--- www/js/utilityMemoQuesCtrl.js
+++ www/js/utilityMemoQuesCtrl.js
@@ -3,4 +3,9 @@ function (Memos,$scope,$state,$http) {
     Memos.GetQues_page().then(function(ques){
         $scope.ques = ques;
     });
-    }]);
+    //跳转
+    $scope.GoContent=function(id){
+        localStorage.setItem(set_question_id,id);
+        $state.go("tab.memo-content");
+    };
+}]);
